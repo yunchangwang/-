8e4c2e6f7b2a89a43e6bf7b9ce77da424ee8e45a
wyc <wyc@wycdemacbook-pro.local>
2016-04-06 09:20:40 +0800

    Initial Commit

---
+++ 图片浏览器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  图片浏览器
+//
+//  Created by wyc on 16/4/6.
+//  Copyright © 2016年 wyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
